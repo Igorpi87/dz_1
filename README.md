3b89f2c546d53d7687112bb19e8b969279f4a666
Igor Ivanov <igorpi87@yandex.ru>
2016-02-02 19:41:55 +0300

    Добавлена форма pop-up

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
- Как установить мой проект
+Как установить мой проект:
 1. git clone https://github.com/Igorpi87/dz1
 2. cd site
 3. bower i
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,38 @@
-$(document).ready(function() {
-	console.log('я на главной странице');
+
+(function  ($) {
+	$(document).ready(function() {
+        console.log('ready');
+    });
+
+// DOM Ready
+      /*  $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#my-button').bind('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#element_to_pop_up').bPopup();
+
+            });
+
+        });    */
+ $(function() {
+            $('#my-button').bind('click', function(e) {
+                e.preventDefault();
+                $('#element_to_pop_up').bPopup({
+                    fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+                    followSpeed: 1500, //can be a string ('slow'/'fast') or int
+                    modalColor: 'black'
+                       
+                });
+            });
+ });
+$('.close-button').on('click', function(){
+    $('#element_to_pop_up').bPopup().close();
 });
+
+}(jQuery))
