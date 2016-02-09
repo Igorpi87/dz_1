aae60204c985f4bff6f78da7448f47257567d077
Igor Ivanov <igorpi87@yandex.ru>
2016-02-09 18:58:14 +0300

    Доработка файлов: add-project.js/php

--- app/js/add_project.js
+++ app/js/add_project.js
@@ -1,62 +1,80 @@
 var myModule = (function () {
-
+   
+   // инициализирует модуль
 	var init = function () {
 		_setUpListners();
 	};
-
+    // прослушивает события
 	var _setUpListners = function () {
 		// прослушка событий
 		$('#new-work-add-popup').on('click', _showModal); // открыть модальное окно
 		$('#new-project-add').on('submit', _addProject); // добавить проект
-		
 	    };
-
+    // работает с модальным окном
 	var _showModal = function (e) {
 		  console.log('Модальное окно');
 		  e.preventDefault();
-		  $('#element_to_pop_up').bPopup({
+		  var divPopup = $('#element_to_pop_up'),
+		      form = divPopup.find('.form');
+		    divPopup.bPopup({
 			modalColor: 'black',
 			speed: 650,
-			transition: 'slideDown'
-		  });
-                  
+			transition: 'slideDown',
+			onClose : function () {
+				form.find('.success-mes').text('').hide(),
+				form.find('.error-mes').text('').hide();
+			}
+		  });               
 	    };
-
+    // добавляет проект
 	var _addProject = function (e) {
 		  console.log('Добавить проект');
 		  e.preventDefault();
 
 		  var form = $(this);
 		      url = 'add-project.php';
-		      data = form.serialize();
+		      serverGiveMeAnAnswer = _ajaxForm( form, url);  
 
-          console.log(data);
-		  // запрос на сервер    
-		  $.ajax({
+		  
+		  serverGiveMeAnAnswer.done(function(ans) {
+		  	console.log(ans);
+
+		  	var successBox = form.find('.success-mes'),
+		  	    errorBox = form.find('.error-mes');
+
+		  	if (ans.status === 'OK'){
+		  		console.log(ans.text);
+		  		successBox.hide();
+		  		successBox.text(ans.text).show();
+		  	}else{
+		  		console.log(ans.text);
+		  		errorBox.hide();
+		  		errorBox.text(ans.text).show();
+		  	}
+		  	
+		  })  
+	    };       
+    // универсальная функция
+    // 1. собирает данные из формы
+    // 2. проверяет форму
+    // 3. делает запрос на сревер и возвращает ответ с сервера
+	var _ajaxForm = function ( form, url ) {
+		// 1. проверить форму
+		// 2. собрать данные из формы
+		// 3. вернуть ответ с сервера
+		 data = form.serialize();
+
+		 var result = $.ajax({
 		  	url: url,
 		  	type: 'POST',
 		  	dataType: 'json',
-		  	data: data
-		  })
-		  .done(function(ans) {
-		  	console.log(ans);
-		  	if (ans.status === 'ok'){
-		  		
-		  		form.find('.success-mes').text(ans.text).show();
-		  	}else{
-		  		form.find('.error-mes').text(ans.text).show();
-		  	}
-		  	
-		  })
-		  .fail(function() {
-		  	console.log("error");
-		  });
-		  
-
-		 
-
-
-	    };       
+		  	data: data,
+		  }).fail ( function(ans) {
+		  	console.log('Проблемы в PHP');
+		  	form.find('.error-mes').text('На сервере произошла ошибка').show();
+    	  });
+		 return result;
+	    };   
 
 	return {
 		init: init
---
+++ app/js/contactModule.js
@@ -0,0 +1,54 @@
+var contactModule = (function(){
+
+	var initial = function () {
+		_setUpListeners();
+	};
+
+	var _setUpListeners = function () {
+		$('#contact-me').on('submit', _submitForm); // отправка формы "связаться со мной"
+	};
+
+	var _submitForm = function (e) {
+		console.log("work with contact form");
+		e.preventDefault();
+		var form = $(this),          
+	      url = 'actions/send-mail.php',
+	      defObject = _ajaxForm(form, url);
+
+	    /*  if (defObject) {
+	        defObject.done(function(ans) {
+	          var mes = ans.mes,
+	              status = ans.status;
+
+	          if ( status === 'OK'){
+	            form.trigger('reset');
+	            form.find('.success-mes').text(mes).show();           
+	          } else{
+	            form.find('.error-mes').text(mes).show();
+	          }
+	        });
+	      }*/
+	};
+
+	var _ajaxForm = function (form, url) {
+	      if (!validator.validationForm(form)) return false;  // Возвращает false, если не проходит валидацию 
+	      var data = form.serialize(); // собираем данные из формы в объект data
+
+	      /*return $.ajax({ // Возвращает Deferred Object
+	        type: 'POST',
+	        url: url,
+	        dataType : 'JSON',
+	        data: data
+	      }).fail( function(ans) {
+	        console.log('Проблемы в PHP');
+	        form.find('.error-mes').text('На сервере произошла ошибка').show();
+	      });*/
+	    };
+
+	return {
+		init: initial
+	};
+
+})();
+
+contactModule.init();
